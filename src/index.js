d3ca7de203adb9875b866eb21fb58ffa87e27231
Vladyslav Mahonia <mahonya333@gmail.com>
2023-01-24 17:00:27 +0200

    feat:algorithm completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix == [] || matrix === undefined) {
+        return [];
+    } else {
+        let resultArray = [];
+        matrix.forEach((element, index) => {
+            resultArray = resultArray.concat(
+                index % 2 !== 0 ? element.reverse() : element
+            );
+        });
+
+        return resultArray;
+    }
+};
